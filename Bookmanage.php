0ec42f7b64e0332006c7d66a12eb211ef3da5509
aparnamohan1234 <mohanaparna465@gmail.com>
2022-05-12 09:29:30 +0530

    login

--- Bookmanage.php
+++ Bookmanage.php
@@ -8,7 +8,7 @@
 </head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <body>
-<table class="table table-borderless">
+
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
